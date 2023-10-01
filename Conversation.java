3cb6853db0963ab329193d1ed9d1a5833b2e6796
Amy Michelle <amy.azogue@fazogue.local>
2023-09-30 23:14:00 -0400

    First full attempt at assignment Chatbot

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,66 @@
-class Conversation {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Conversation {
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+		Scanner input = new Scanner(System.in);
+
+// Reads an int value for the number of rounds for the conversation
+    System.out.print("Enter the number of rounds: ");
+		int numRounds = input.nextInt();
+
+// Clears buffer by reading the nect line
+		input.nextLine(); 
+
+    //Creation of transcript to hold on to the whole interaction
+    StringBuilder transcript = new StringBuilder();
+
+
+// Welcoming statement
+    System.out.print("Awesome! Hello, Lets get started. What's on your mind? \n");
+    transcript.append("Program: ").append("Awesome! Hello, Lets get started. What's on your mind? ").append("\n");
+
+
+// You will start the conversation here.
+
+    //Base response statements
+    String[] cannedResponses = { "uh-huh", "interesting", "mhm", "I see...", "*GASP*" };
+    Random random = new Random();
+
+
+
+// Loop to start conversation
+    for (int round = 1; round <= numRounds; round++) {
+      String userResponse = input.nextLine();
+      transcript.append("User: ").append(userResponse).append("\n");
+
+
+      if (userResponse.toLowerCase().contains("I ")) {
+        String mirResponse = userResponse.replace("I ", "You ");
+        System.out.print(mirResponse);
+        transcript.append("Program: ").append(mirResponse).append("\n");
+
+// Prints random responses from cannedResponses
+      } else {
+          int randomIndex = random.nextInt(cannedResponses.length);
+          String randomResponse = cannedResponses[randomIndex];
+          System.out.print(randomResponse);
+          System.out.print("\n");
+          transcript.append("Program: ").append(randomResponse).append("\n");
+  }
+
+} 
+
+// Goodbye statement
+        System.out.print("It was lovely talking with you! Have a nice day ;) \n ");
+        transcript.append("Program - ").append("It was lovely talking with you! Have a nice day ;)").append("\n");
+
+//Print Transcript
+        System.out.print("----------Transcript----------");
+        System.out.print("\n");
+        System.out.print(transcript);
+
+// Closing the scanner just for good practice
+        input.close();
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+None, i'm still pretty intimidated by the upper classmen.
 - Which session(s) of TA / office hours did you attend?
+I attended wednesday sept. 27 TA hours from 7-9 pm
 - What are your initial impressions of Java? 
+I am not a fan. I'm doing the transition from Python and Java seems to get angry with me for the smallest reasons and typos.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+Programming is heavily based on logic and will be infinitely easier through flowcharts, reading documentation, and just a lot of practice. There is a tradeoff between Python and Java where in Python its more user friendly you dont have as mush free range to mess around and find out and learn as openly as in Java.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+READ THE DOCUMENTATION AND GO TO OFFICE AND TA HOURS. I'll more likely than not have to go again to keep tweaking and improving my code. ALSO PLEASE WRITE AS MUCH COMMENTS AS POSSIBLE TO KEEP YOUR PROGRAM ORGANIZED.
